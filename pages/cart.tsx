cd1d9bfbf31d46c219a3677e1e2570b782b60dbe
oterte <97093479+oterte@users.noreply.github.com>
2023-06-09 22:09:20 +0900

    스키마 변경, cart 조회시 join 추가

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,53 +2,40 @@ import { CountControl } from "@/components/CountControl";
 import { CATEGORY_MAP } from "@/constants/products";
 import styled from "@emotion/styled";
 import { Button } from "@mantine/core";
-import { products } from "@prisma/client";
+import { Cart, products } from "@prisma/client";
 import { IconRefresh, IconX } from "@tabler/icons-react";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useMemo, useState } from "react";
-interface CartItem {
+interface CartItem extends Cart {
   name: string;
   productId: number;
-  amount: number;
-  price: number;
   image_url: string;
-  cost: number;
 }
-
-function Cart() {
-  const [cart, setCart] = useState<CartItem[]>([]);
-
-  const deliPay = 3000;
+// interface CartItem {
+//   name: string;
+//   productId: number;
+//   amount: number;
+//   price: number;
+//   image_url: string;
+//   cost: number;
+// }
+function CartPage() {
+  const { data } = useQuery<{ items: CartItem[] }, unknown, CartItem[]>(
+    [`/api/get-cart`],
+    () => fetch(`/api/get-cart`).then((res) => res.json()).then((data) => data.items)
+  );
+  const deliPay = data && data.length > 0 ? 3000: 0;
   const discount = 0;
   const totalCost = useMemo(() => {
-    return cart.map((item) => item.cost).reduce((prev, curr) => prev + curr, 0);
-  }, [cart]);
+    if(data == null){
+      return 0
+    }
+    return data.map((item) => item.price).reduce((prev, curr) => prev + curr, 0);
+  }, [data]);
   const router = useRouter();
-  useEffect(() => {
-    const mockData = [
-      {
-        name: "멋들어진 신발",
-        productId: 56,
-        amount: 1,
-        price: 20000,
-        cost: 20000,
-        image_url:
-          "https://cdn.shopify.com/s/files/1/0282/5850/products/footwear_nike_air-force-1-low-retro_DJ3911-100.view_1_720x.jpg",
-      },
-      {
-        name: "멋들어진 모자",
-        productId: 89,
-        amount: 3,
-        price: 25000,
-        cost: 75000,
-        image_url:
-          "https://cdn.shopify.com/s/files/1/0282/5850/products/accessories_headwear_undefeated_wave-strapback_90218.color_teal.view_1_720x.jpg",
-      },
-    ];
-    setCart(mockData);
-  }, []);
+
   const { data: products } = useQuery<
     { items: products[] },
     unknown,
@@ -66,11 +53,11 @@ function Cart() {
   };
   return (
     <div>
-      <span className="text-2xl mb-3">Cart ({cart.length})</span>
+      <span className="text-2xl mb-3">Cart ({data ? data.length : 0})</span>
       <div className="flex">
         <div className="flex flex-col p-4 space-y-4 flex-1">
-          {cart.length > 0 ? (
-            cart.map((item, idx) => <Item key={idx} {...item} />)
+          {data && data.length > 0 ? (
+            data.map((item, idx) => <Item key={idx} {...item} />)
           ) : (
             <div>장바구니에 아무것도 없습니다.</div>
           )}
@@ -150,12 +137,12 @@ function Cart() {
   );
 }
 
-export default Cart;
+export default CartPage;
 
 const Item = (props: CartItem) => {
   const router = useRouter();
-  const [quantity, setQuantity] = useState<number | undefined>(props.amount);
-  const [cost, setCost] = useState<number>(props.amount);
+  const [quantity, setQuantity] = useState<number | undefined>(props.quantity);
+  const [cost, setCost] = useState<number>(props.price);
   useEffect(() => {
     if (quantity != null) {
       setCost(quantity * props.price);
